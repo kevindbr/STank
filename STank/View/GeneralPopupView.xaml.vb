ced93c0fa8ba22f12ff4808c5d56860e6ebc49a6
Ben Johnke <ben.johnke@colostate.edu>
2016-08-08 00:19:28 -0600

    Adding general popup

---
+++ STank/View/GeneralPopupView.xaml.vb
@@ -0,0 +1,29 @@
+﻿
+Class GeneralPopupView
+
+    Public mMainViewModel As MainViewModel
+    Public mFindAndReplaceMainViewModel As FindAndReplaceMainViewModel
+    Public runClicked As Boolean
+
+
+    ''' <summary>
+    ''' Send message show message
+    ''' </summary>
+    ''' <param name="message"></param>
+    ''' <remarks></remarks>
+    Sub New(message As String)
+        InitializeComponent()
+        messageView.Text = message
+
+    End Sub
+
+    Private Sub exitView(sender As Object, e As RoutedEventArgs)
+        Close()
+    End Sub
+
+
+    Private Sub IntializeWindow()
+
+    End Sub
+
+End Class
